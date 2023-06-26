f7cb2428e865fdb11b400d5e311e4b7367cf02b0
Jarmille <jarmille@umich.edu>
2023-06-26 19:49:48 -0400

    Added 404 hack for GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,34 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import wasm from 'vite-plugin-wasm'
+
+/**
+ * @param newFilename {string}
+ * @returns {import('vite').Plugin}
+ */
+const githubPagesHack = () => {
+  return {
+    name: 'renameIndex',
+    enforce: 'post',
+    generateBundle(options, bundle) {
+      // Duplicate our index to 404 so we can use history routing
+      bundle['404.html'] = {
+        ...bundle['index.html'],
+        fileName: '404.html',
+      }
+      
+    },
+  } as PluginOption
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     wasm(),
+    githubPagesHack(),
   ],
   resolve: {
     alias: {
